257f43e6a92fbef9bce1388a3dc53dfa93bcacd7
Guilherme Sonnemann Eckert <sonnermanneckertg@student.douglascollege.ca>
2020-10-21 23:32:44 -0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextFirstName = new System.Windows.Forms.TextBox();
+            this.TextLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btn_Process = new System.Windows.Forms.Button();
+            this.RadioDeposit = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Total = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextFirstName
+            // 
+            this.TextFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextFirstName.Location = new System.Drawing.Point(26, 52);
+            this.TextFirstName.Name = "TextFirstName";
+            this.TextFirstName.Size = new System.Drawing.Size(216, 31);
+            this.TextFirstName.TabIndex = 0;
+            // 
+            // TextLastName
+            // 
+            this.TextLastName.Location = new System.Drawing.Point(30, 52);
+            this.TextLastName.Name = "TextLastName";
+            this.TextLastName.Size = new System.Drawing.Size(224, 31);
+            this.TextLastName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TextFirstName);
+            this.groupBox1.Location = new System.Drawing.Point(129, 178);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 130);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "First Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TextLastName);
+            this.groupBox2.Location = new System.Drawing.Point(472, 178);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 130);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Last Name";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Items.AddRange(new object[] {
+            "list"});
+            this.listBox1.Location = new System.Drawing.Point(885, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 412);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Maroon;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(361, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(600, 58);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Your Name financial Account";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Btn_Process
+            // 
+            this.Btn_Process.Location = new System.Drawing.Point(511, 540);
+            this.Btn_Process.Name = "Btn_Process";
+            this.Btn_Process.Size = new System.Drawing.Size(205, 43);
+            this.Btn_Process.TabIndex = 7;
+            this.Btn_Process.Text = "Process";
+            this.Btn_Process.UseVisualStyleBackColor = true;
+            this.Btn_Process.Click += new System.EventHandler(this.Btn_Process_Click);
+            // 
+            // RadioDeposit
+            // 
+            this.RadioDeposit.AutoSize = true;
+            this.RadioDeposit.Checked = true;
+            this.RadioDeposit.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RadioDeposit.Location = new System.Drawing.Point(174, 398);
+            this.RadioDeposit.Name = "RadioDeposit";
+            this.RadioDeposit.Size = new System.Drawing.Size(139, 36);
+            this.RadioDeposit.TabIndex = 8;
+            this.RadioDeposit.TabStop = true;
+            this.RadioDeposit.Text = "Deposit";
+            this.RadioDeposit.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(502, 398);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(182, 36);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.Text = "Withdrawal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Total
+            // 
+            this.Total.BackColor = System.Drawing.Color.DarkCyan;
+            this.Total.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Total.ForeColor = System.Drawing.Color.Gold;
+            this.Total.Location = new System.Drawing.Point(885, 661);
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            this.Total.Size = new System.Drawing.Size(343, 39);
+            this.Total.TabIndex = 10;
+            this.Total.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 804);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.RadioDeposit);
+            this.Controls.Add(this.Btn_Process);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Financial Account";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextFirstName;
+        private System.Windows.Forms.TextBox TextLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn_Process;
+        private System.Windows.Forms.RadioButton RadioDeposit;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox Total;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        decimal number;
+        decimal Totaldeposits;
+        decimal Totalwithdrawal;
+        decimal Totalaccount;
+
+        string path = @"bin\debug\TransactionError.dat";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Btn_Process_Click(object sender, EventArgs e)
+        {
+
+
+            listBox1.Items.Clear();
+            Total.Text = String.Empty;
+            Totaldeposits = 0;
+            Totalwithdrawal = 0;
+            Totalaccount = 0;
+            Total.BackColor = Color.DarkCyan;
+            Total.ForeColor = Color.Gold;
+            listBox1.Items.Add(TextFirstName.Text.ToLower() + " " + TextLastName.Text.ToLower());
+            listBox1.Items.Add("=======================");
+            loadCustomer();
+
+         
+            if (RadioDeposit.Checked == true )
+            {
+                listBox1.Items.Add("=====================");
+                listBox1.Items.Add("Total Deposits =" + Totaldeposits.ToString("C"));
+                listBox1.Items.Add("Total Account =" + Totalaccount.ToString("C"));
+            } if (RadioDeposit.Checked == false )
+            {
+                listBox1.Items.Add("=====================");
+                listBox1.Items.Add("Total Withdrawal =" + Totalwithdrawal.ToString("C"));
+                listBox1.Items.Add("Total Account =" + Totalaccount.ToString("C"));
+            }
+           
+
+
+        }
+
+
+
+        private void loadCustomer()
+        {
+
+            try
+            {
+
+                using (var reader = new StreamReader(@"C:\Users\Guilherme\Desktop\DOc\Customer.dat"))
+                {
+
+                    int counter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        string fullName = line.Trim();
+                        var names = fullName.Split(' ');
+
+
+                        if (names.Contains(TextFirstName.Text.ToLower()) || names.Contains(TextLastName.Text.ToLower()))
+                        {
+                            loadTransactions(counter);
+                        } 
+
+                        counter++;
+                    } 
+                    
+                    reader.Close();
+
+                }
+            } catch (Exception)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
+        }
+
+
+        private void loadTransactions(int counter)
+        {
+
+            var line = File.ReadLines(@"C:\Users\Guilherme\Desktop\DOc\Transaction.dat").Skip(counter).Take(1).First();
+
+            NumberStyles style;
+            CultureInfo culture;
+
+            style = NumberStyles.Number;
+            culture = CultureInfo.CreateSpecificCulture("en-US");
+
+            try {
+
+                if (Decimal.TryParse(line.Trim(), style, culture, out number))
+                {
+
+
+                    if (number > 0 && RadioDeposit.Checked == true)
+                    {
+
+                        listBox1.Items.Add(number.ToString("C"));
+                        Totaldeposits += number;
+
+
+                    }
+
+                    if (number < 0 && RadioDeposit.Checked == false)
+                    {
+                        listBox1.Items.Add(number.ToString("C"));
+                        Totalwithdrawal += number;
+
+                    }
+
+                }
+
+                else
+                {
+                    var name = File.ReadLines(@"C:\Users\Guilherme\Desktop\DOc\Customer.dat").Skip(counter).Take(1).First();
+                    File.AppendAllText(@"C:\Users\Guilherme\Desktop\DOc\TransactionError.dat", name + " "+ line + Environment.NewLine);
+                }
+
+                Totalaccount += number;
+
+
+                if (Totalaccount < 0)
+                {
+
+                    Total.BackColor = Color.Red;
+                    Total.ForeColor = Color.Black;
+                    Total.Text = "(" + Totalaccount.ToString("C") + ")";
+                }
+
+                if (Totalaccount > 0)
+                {
+
+                    Total.Text = Totalaccount.ToString("C");
+                }
+            } catch (Exception) {
+                MessageBox.Show("Something went wrong");
+            }
+
+        } 
+
+    }
+ }
+
